7dc91cf39700e74c2c633f3c66e5e70e79b81114
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-08 19:49:20 -0300

    add exerc corrigido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿  Console.WriteLine("Não Esmague a aranha,cuidado");
+            Console.ReadLine();
+            
+            Console.WriteLine("Atenção!");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(" ||  || ");
+            Console.WriteLine(" \\\\()// ");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+            Console.ReadKey();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Você não esmagou a aranha parabens :D");
